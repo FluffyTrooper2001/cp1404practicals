534143a4db3e9b0ddc9b0f1c6769f6b4b66409e9
Michael <michael.richards3@my.jcu.edu.au>
2020-11-01 14:59:18 +1000

    complete file sort 1

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -4,16 +4,24 @@ import shutil
 def main():
     os.chdir("FilesToSort")
     sort_dict = {}
+    extensions = []
     for filename in os.listdir('.'):
         extension = filename[filename.find('.')+1:]
-        if extension not in os.listdir('.'):
+        if extension not in extensions:
+            extensions.append(extension)
             sort_dict.update({extension:[filename]})
         else:
             sort_dict[extension].append(filename)
+    print(sort_dict)
     for dir_name, filenames in sort_dict.items():
+        print(f"Moving {filenames} to {dir_name}")
         try:
             os.mkdir(dir_name)
+            print(dir_name, "made")
         except:
             pass
         for filename in filenames:
             shutil.move(filename, dir_name)
+            print(filename, "moved to", dir_name)
+
+main()
